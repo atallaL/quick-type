82096ec5fd0c6afb4f6d9447a90a504c16a2ded2
Leonardo Atalla <latal094@uottawa.ca>
2025-06-19 18:10:01 -0400

    basic structure + home page visuals half implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import logo from './assets/logo.png'
+
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="mainContainer">
+        <div className="homeContainer">
+          <div className="homeCenter">
+            <div className="logoContainer">
+              <img src={logo} />
+            </div>
+            <div className="homeStart">
+              <div className="startButton">
+                <h2>start</h2>
+                <i class="bi bi-caret-right-fill"></i>
+              </div>
+              <p>how do i play?</p>
+            </div>
+          </div>
+          <div className="homeBottom">
+            <div className="difficultySelection">
+              <p>hi</p>
+            </div>
+          </div>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  )  
 }
 
 export default App
