558cdd3252c87c4675bb736c7bb09c1e039b0a61
Leonardo Atalla <latal094@uottawa.ca>
2025-06-20 17:28:56 -0400

    functionality complete

--- src/components/endscreen/EndScreen.jsx
+++ src/components/endscreen/EndScreen.jsx
@@ -1,30 +1,50 @@
 import './EndScreen.css'
 
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import {playAudio} from '../../utils/soundPlayer'
 
-export default function EndScreen({stats}) {
+export default function EndScreen({setGameState, stats}) {
 
+    //choose a random word from this list to display
     const endWordList = ['great job!', 'incredible!', 'well done!', 'impressive!', 'amazing!', 'fantastic!']
     const [endWord, setEndWord] = useState('');
     useEffect(() => {
         setEndWord(endWordList[Math.floor(Math.random() * endWordList.length)]);
     }, [])
 
+    const endButtonsRef = useRef(null); //to make them uninteractable until thingy is done
+
+    //on start, remove animating class from thingy
+    useEffect(() => {
+        setTimeout(() => {
+            endButtonsRef.current.classList.remove('hiding');
+        }, 2500);
+    }, []);
 
     return (
         <div className="endContainer">
-            <div className="endHeader">
-                <h2>{endWord}</h2>
+            <div className="endText">
+                <div className="endHeader">
+                    <h2>{endWord}</h2>
+                </div>
+                <div className="endStats">
+                    <h3>stats:</h3>
+                    <p>total time elapsed: {stats.timeElapsed}s</p>
+                    <p>average reaction time: {stats.wordsCompleted === 0 ? 'N/A' : Math.floor(stats.timeFirstInputSum / stats.wordsCompleted)}</p>
+                    <p>total words typed: {stats.wordsCompleted}</p>
+                </div>
             </div>
-            <div className="endStats">
-                {stats.wordsCompleted}
-            </div>
-            <div className="endButtons">
-                {stats.wordsCompleted}
+            <div className="endButtons hiding" ref={endButtonsRef}>
+                <div className="endButton endHome" onClick={() => setGameState('home')}>
+                    <p>home</p>
+                </div>
+                <div className="endButton endRetry" onClick={() => setGameState('game')}>
+                    <p>retry</p>
+                    <i className="bi bi-caret-right-fill"></i>
+                </div>
             </div>
         </div>
     );
