9cc9d0559d4d810f1721b08d30d0da5c1980414d
Leonardo Atalla <leonardoyatalla@gmail.com>
2025-06-20 15:26:53 -0400

    endscreen structure

--- src/components/endscreen/EndScreen.jsx
+++ src/components/endscreen/EndScreen.jsx
@@ -1,14 +1,31 @@
 import './EndScreen.css'
 
+import {useState, useEffect} from 'react';
+
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import {playAudio} from '../../utils/soundPlayer'
 
 export default function EndScreen({stats}) {
-    
+
+    const endWordList = ['great job!', 'incredible!', 'well done!', 'impressive!', 'amazing!', 'fantastic!']
+    const [endWord, setEndWord] = useState('');
+    useEffect(() => {
+        setEndWord(endWordList[Math.floor(Math.random() * endWordList.length)]);
+    }, [])
+
+
     return (
         <div className="endContainer">
-            {stats.wordsCompleted}
+            <div className="endHeader">
+                <h2>{endWord}</h2>
+            </div>
+            <div className="endStats">
+                {stats.wordsCompleted}
+            </div>
+            <div className="endButtons">
+                {stats.wordsCompleted}
+            </div>
         </div>
     );
 }
